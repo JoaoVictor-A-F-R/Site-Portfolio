a6db774e19ddcddf04343a310bf1e65700ad2260
JoaoVictor-A-F-R <joaovictoraraujoflavioribeiro@gmail.com>
2023-10-17 12:00:08 -0300

    responsividade mobile

--- javaScript/menu.js
+++ javaScript/menu.js
@@ -2,4 +2,3 @@ const botaoMenu = document.querySelector('.iconMenu')
 const menu = document.querySelector('.menu')
 
 botaoMenu.addEventListener('click', () => menu.classList.toggle('ativar'))
-console.log(menu.childNodes)
